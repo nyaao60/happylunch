07978187ace2f6fb9a30919915209986b55967ed
nao-6028 <na.ha60283@gmail.com>
2023-01-15 23:23:52 +0900

    wip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,9 +20,30 @@ class PostsController < ApplicationController
     @user=User.find_by(id:@post.user_id)
   end
   
+  def edit
+    @post=Post.find(params[:id])
+  end  
+
+  def update
+    @post=Post.find(params[:id])      
+    if @post.update(post_params)
+      flash[:success]="投稿を更新しました"
+      redirect_to @post
+    else
+      render 'edit'
+    end 
+  end  
+
+  def destroy
+    @post=Post.find(params[:id])
+    if @post.destroy
+    flash[:success]="投稿を削除しました" 
+    redirect_to root_url
+    end
+  end
+  
   private
     def post_params
       params.require(:post).permit(:store_name,:adress, {post_images:[]},:price,:five_star_rating,:lots_of_vegetables,:body)
     end
 end
-
