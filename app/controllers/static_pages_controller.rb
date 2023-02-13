6bc2301fb0981beaf0ac8c37519f37648e88488a
nao-6028 <na.ha60283@gmail.com>
2023-02-13 21:32:29 +0900

    TOPページに投稿一覧表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
+
   def home
+    user_ids = current_user.following.pluck(:id)
+    user_ids.push(current_user.id)
+    @posts=Post.where(user_id:user_ids).page(params[:page])
   end
+
 end
