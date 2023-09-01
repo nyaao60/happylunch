4f79dbc5093bcf9263b96bf726751dcfa2ec1348
nao-6028 <na.ha60283@gmail.com>
2023-09-01 12:39:08 +0900

    TOP画面の機能追加（検索条件）、レイアウト修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,14 +6,12 @@ class Post < ApplicationRecord
   validates :store_name, presence: true , length: { maximum: 30} 
   validates :lunch_name, presence: true , length: { maximum: 30} 
   validates :address, presence: true,length: { maximum: 60}
-  validates :price, presence: true,numericality: {  greater_than_or_equal_to: 1,less_than_or_equal_to: 800 }
+  validates :price, presence: true,numericality: {  greater_than_or_equal_to: 1,less_than_or_equal_to: 1500 }
   validates :body, presence: true,length: { maximum: 400}
   validates :post_images,presence: true
   mount_uploaders :post_images, ImageUploader
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
-  # # reverse_geocoded_by :latitude, :longitude
-  # after_validation :reverse_geocode
 
   def liked_by(user_id)
     likes.where(user_id: user_id).exists?
