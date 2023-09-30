5a43bd761d931d9026c1d390812c1e8c3556d702
nao-6028 <na.ha60283@gmail.com>
2023-09-30 14:13:00 +0900

    home画面検索フォームにキーワード検索、人気のタグ欄を設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
 
   def home
-    return @posts = Post.includes(:user,:likes,:comments).order(created_at: :desc) if !logged_in? || current_user.following.count == 0
-    user_ids = current_user.following.pluck(:id)
-    user_ids.push(current_user.id)
-    @posts=Post.includes(:user,:likes,:comments).where(user_id:user_ids).order(created_at: :desc).page(params[:page])
-  end
-  
+    @tag_list = Tag.find(PostTagRelation.group(:tag_id).order('count(tag_id) desc').limit(7).pluck(:tag_id))
+    
+    if logged_in? && current_user.following.count > 0
+      user_ids = current_user.following.ids << current_user.id
+      @posts = Post.includes(:user, :likes, :comments, :tags).where(user_id: user_ids).order(created_at: :desc).page(params[:page])
+    else
+      @posts = Post.includes(:user, :likes, :comments, :tags).order(created_at: :desc)
+    end
+  end  
+
 end
