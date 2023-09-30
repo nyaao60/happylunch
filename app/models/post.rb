5a35472b30b8a2174535844989babccfec87fe47
nao-6028 <na.ha60283@gmail.com>
2023-09-30 14:54:23 +0900

    postモデルのvalidates文修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
   validates :store_name, presence: true , length: { maximum: 30} 
   validates :lunch_name, presence: true , length: { maximum: 30} 
   validates :address, presence: true,length: { maximum: 60}
-  validates :price, presence: true,numericality: { within: 1..1500 }
+  validates :price, presence: true,numericality: {  greater_than_or_equal_to: 1,less_than_or_equal_to: 1500 }
   validates :body, presence: true,length: { maximum: 400}
   validates :post_images,presence: true
 
